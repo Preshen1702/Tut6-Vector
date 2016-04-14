bf7147542b0d38c3fbbddc71eb16016eba65c922
Preshen1702 <214532872@stu.ukzn.ac.za>
2016-04-14 21:48:07 +0400

    Vector for 1 - 50

---
+++ Tut6-Vector/Vector.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main() {
+	vector <int> Vector;
+	int i;
+	for (i = 50; i > 0; i--) {
+		Vector.push_back(i);
+	}
+	sort(Vector.begin(), Vector.end());
+
+	for (i = 0; i < 50; i++) {
+		cout << Vector.at(i) << endl;
+	}
+	return 0;
+}
