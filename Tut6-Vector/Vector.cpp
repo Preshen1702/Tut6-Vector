39aca6f25cad7b80768f02f2ca9801c076583b6c
Preshen1702 <214532872@stu.ukzn.ac.za>
2016-04-14 21:48:19 +0400

    Reversing the order

--- Tut6-Vector/Vector.cpp
+++ Tut6-Vector/Vector.cpp
@@ -9,7 +9,7 @@ int main() {
 		Vector.push_back(i);
 	}
 	sort(Vector.begin(), Vector.end());
-
+	reverse(Vector.begin(), Vector.end());
 	for (i = 0; i < 50; i++) {
 		cout << Vector.at(i) << endl;
 	}
